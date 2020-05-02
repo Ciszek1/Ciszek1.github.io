ccb2779a349636e107ecabda1c39ba566fffa667
Ciszek1 <61918655+Ciszek1@users.noreply.github.com>
2020-05-02 14:47:22 +0200

    Update index.js

--- eagle space/kontrol/inne/map/index.js
+++ eagle space/kontrol/inne/map/index.js
@@ -2,7 +2,7 @@ var map = L.map('map', {
     center: [10, 0],
     zoom: 2
 });
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+var Stadia_AlidadeSmoothDark = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
